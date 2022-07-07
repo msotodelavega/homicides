91100e1b30b62b0d4ce5f98b4f90fce5660acd23
msotodelavega <105311939+msotodelavega@users.noreply.github.com>
2022-07-07 07:35:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import dash
 import dash_labs as dl
 import dash_bootstrap_components as dbc
 
-app = dash.Dash(
+dash_app = dash.Dash(
     __name__, plugins=[dl.plugins.pages], external_stylesheets=[dbc.themes.MINTY]
 )
-
+app = dash_app.server
 dl.print_registry()
 navbar = dbc.NavbarSimple(
     dbc.Nav(
@@ -21,10 +21,10 @@ navbar = dbc.NavbarSimple(
     className="mb-2",
 )
 
-app.layout = dbc.Container(
+dash_app.layout = dbc.Container(
     [navbar, dl.plugins.page_container],
     fluid=True,
 )
 
 if __name__ == "__main__":
-    app.run_server(host='20.119.0.18', port='80', debug=True)
+    dash_app.run_server(host='20.119.0.18', port='80', debug=True)
